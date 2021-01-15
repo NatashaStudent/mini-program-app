e55502b194f8c861d6d32180e521ffdc526d108e
Rory Welsh <rorywelsh5@gmail.com>
2021-01-15 13:48:01 +0200

    get service working initial

--- feathers-services/src/sequelize.ts
+++ feathers-services/src/sequelize.ts
@@ -1,32 +1,38 @@
-import { Sequelize } from 'sequelize';
-import { Application } from './declarations';
+import { Sequelize } from "sequelize";
+import { Application } from "./declarations";
 
-export default function (app: Application): void {
-  const connectionString = app.get('postgres');
-  const sequelize = new Sequelize(connectionString, {
-    dialect: 'postgres',
-    logging: false,
-    define: {
-      freezeTableName: true
+export default function configureSequelize(app: Application): void {
+  const pgOptions = app.get("postgres");
+
+  const sequelize = new Sequelize(
+    pgOptions.database,
+    pgOptions.username,
+    pgOptions.password,
+    {
+      dialect: "postgres",
+      dialectOptions: { useUTC: true },
+      timezone: "+02:00",
+      define: { underscored: true },
+      host: pgOptions.host,
     }
-  });
+  );
   const oldSetup = app.setup;
 
-  app.set('sequelizeClient', sequelize);
+  app.set("sequelizeClient", sequelize);
 
   app.setup = function (...args): Application {
     const result = oldSetup.apply(this, args);
 
     // Set up data relationships
     const models = sequelize.models;
-    Object.keys(models).forEach(name => {
-      if ('associate' in models[name]) {
+    Object.keys(models).forEach((name) => {
+      if ("associate" in models[name]) {
         (models[name] as any).associate(models);
       }
     });
 
     // Sync to the database
-    app.set('sequelizeSync', sequelize.sync());
+    app.set("sequelizeSync", sequelize.sync());
 
     return result;
   };
